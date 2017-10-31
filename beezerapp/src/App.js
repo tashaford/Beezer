d4a8857404b52171c19e524bad67d38452c1277a
Natasha Ford <neford3991@gmail.com>
2017-10-31 12:42:19 +0000

    Small changes made to app.js, to remove some of the react set up

--- beezerapp/src/App.js
+++ beezerapp/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import * as firebase from "firebase";
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,11 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Accounts</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        All of the information
         </p>
       </div>
     );
